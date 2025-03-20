993f9c35fbd35b30b23ec79ddab6d983d83df7b4
hinamimi <hinamimi09@gmail.com>
2025-03-21 01:59:35 +0900

    debug

--- src/render.tsx
+++ src/render.tsx
@@ -1,25 +1,27 @@
-import { createRoot } from "https://esm.sh/react-dom@19.0.0/client?bundle";
-import { Body } from "./components/Body.tsx";
+// import { createRoot } from "https://esm.sh/react-dom@19.0.0/client?bundle";
+// import { Body } from "./components/Body.tsx";
 import process from "https://esm.sh/process@0.11.10";
 
 globalThis.process = process;
 
-const execRender = () => {
-  createRoot(document.querySelector("body")!).render(
-    <>
-      {Body(location.pathname)}
-    </>,
-  );
-};
+console.log("render.tsx");
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded");
-    console.log(Body(location.pathname));
-    execRender();
-  });
-} else {
-  console.log("Script loaded after DOMContentLoaded");
-  console.log(Body(location.pathname));
-  execRender();
-}
+// const execRender = () => {
+//   createRoot(document.querySelector("body")!).render(
+//     <>
+//       {Body(location.pathname)}
+//     </>,
+//   );
+// };
+
+// if (document.readyState === "loading") {
+//   document.addEventListener("DOMContentLoaded", () => {
+//     console.log("DOMContentLoaded");
+//     console.log(Body(location.pathname));
+//     execRender();
+//   });
+// } else {
+//   console.log("Script loaded after DOMContentLoaded");
+//   console.log(Body(location.pathname));
+//   execRender();
+// }
